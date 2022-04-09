1ebc5dc155a25daa1474290aad200cfe4fd6bfc7
Bill-Yeh <lawyer112240@gmail.com>
2022-04-09 16:54:22 +0800

    rwd index page
    活動專區部分需再加上特效

---
+++ javascript/rwd-footer.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $(".product-service").click(function() {
+        $(".product-list").slideToggle();
+    })
+
+    $(".promotion-activity").click(function() {
+        $(".promotion-list").slideToggle();
+    })
+
+    $(".service-info").click(function() {
+        $(".service-list").slideToggle();
+    })
+
+    $(".about").click(function() {
+        $(".about-list").slideToggle();
+    })
+})
