1adcd5a99220b5dd9a34a7046018544c3721e659
Bill-Yeh <lawyer112240@gmail.com>
2022-04-22 00:57:19 +0800

    choosing type page
    添加圖片變化、顏色變化效果，另外修改顏色選取和儲存空間選取的邊框效果

---
+++ javascript/detailed-problem.js
@@ -0,0 +1,18 @@
+function $id(id) {
+    return document.getElementById(id);
+}
+
+function problemForm(e) {
+    let problemType = $id("problem");
+
+    if (problemType.selectedIndex == 0) {
+        alert("請選擇問題類型");
+        return;
+    }
+
+    if (problemType.selectedIndex != 0) return window.location.replace('/online-repair-appointment-finish.html');
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("problem-submit").onsubmit = problemForm;
+});
