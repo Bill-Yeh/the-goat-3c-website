0cc723c6628d665921434e7a5850347d651e8ac5
Bill-Yeh <lawyer112240@gmail.com>
2022-04-23 00:13:04 +0800

    choosing type page
    再添加一個圖卡有圖片變換功能

--- javascript/choosing_type.js
+++ javascript/choosing_type.js
@@ -1,12 +1,19 @@
 //點擊按鈕切換手機圖片和顏色名稱
 
-window.addEventListener("load",function(){
+window.addEventListener("load", function() {
+    // 按鈕
     let colorBtn = document.getElementsByClassName("colorMenu");
-    let phonePicture = document.getElementsByClassName("smartPhone");
-    let colorText = this.document.getElementsByClassName("color-txt");
-   
+    let colorBtn2 = document.getElementsByClassName("colorMenu2");
 
-    for(let i = 0; i<colorBtn.length; i++){
+    // 圖片
+    let phonePicture = document.getElementsByClassName("smartPhone");
+    let phonePicture2 = document.getElementsByClassName("smartPhone2");
+    // 文字
+    let colorText = this.document.getElementsByClassName("color-txt");
+    let colorText2 = this.document.getElementsByClassName("color-txt2");
+
+    // 圖卡1
+    for (let i = 0; i < colorBtn.length; i++) {
         // 顏色名稱
         colorText[i].style.display = "none";
         colorText[0].style.display = "block";
@@ -15,13 +22,35 @@ window.addEventListener("load",function(){
         phonePicture[i].style.display = "none";
         phonePicture[0].style.display = "block";
 
-        colorBtn[i].addEventListener("click",function(){
+        colorBtn[i].addEventListener("click", function() {
             colorText[i].style.display = "block";
             phonePicture[i].style.display = "block";
-            for(let j=0; j<colorBtn.length; j++){
-                if(i != j){
+            for (let j = 0; j < colorBtn.length; j++) {
+                if (i != j) {
                     phonePicture[j].style.display = "none";
                     colorText[j].style.display = "none";
+                }
+            }
+        });
+    };
+
+    // 圖卡2
+    for (let i = 0; i < colorBtn2.length; i++) {
+        // 顏色名稱
+        colorText2[i].style.display = "none";
+        colorText2[0].style.display = "block";
+
+        // 手機圖片
+        phonePicture2[i].style.display = "none";
+        phonePicture2[0].style.display = "block";
+
+        colorBtn2[i].addEventListener("click", function() {
+            colorText2[i].style.display = "block";
+            phonePicture2[i].style.display = "block";
+            for (let j = 0; j < colorBtn2.length; j++) {
+                if (i != j) {
+                    phonePicture2[j].style.display = "none";
+                    colorText2[j].style.display = "none";
                 }
             }
         });
--- javascript/register.js
+++ javascript/register.js
@@ -1,67 +1,67 @@
-function $id(id){
+function $id(id) {
     return document.getElementById(id);
-} 
+}
 
-function registerForm(e){
+function registerForm(e) {
     let newAccount = $id("user-register");
     let newPassword = $id("password-register");
     let confirmPassword = $id("password-confirm");
 
     // 檢查帳號和密碼欄位是否空白
-    if(newAccount.value=="" || newPassword.value == "" || confirmPassword.value == ""){
+    if (newAccount.value == "" || newPassword.value == "" || confirmPassword.value == "") {
         alert("帳號、密碼與確認密碼欄位不得空白");
         e.preventDefault();
         return;
     }
 
     let accountReg = /(^09\d{8}$)|(^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$)/; //email或手機號碼的正規表達式
-    let passwordReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;  //密碼的正規表達式
+    let passwordReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/; //密碼的正規表達式
     let AccountValid = accountReg.test(newAccount.value); //帳號過
     let PasswordValid = passwordReg.test(newPassword.value); //密碼過
     let confirmId = newPassword.value; //確認密碼
 
-    console.log("帳號",AccountValid);
-    console.log("密碼",PasswordValid);
-    console.log("確認密碼",confirmId);
+    console.log("帳號", AccountValid);
+    console.log("密碼", PasswordValid);
+    console.log("確認密碼", confirmId);
 
     // 帳號驗證
-    if (newAccount.value.search(accountReg)==-1){
+    if (newAccount.value.search(accountReg) == -1) {
         document.getElementById("register-account-tips").innerText = "帳號格式錯誤";
-        document.getElementById("register-account-tips").style.color ="red";
-        newAccount.value ="";
+        document.getElementById("register-account-tips").style.color = "red";
+        newAccount.value = "";
         newAccount.focus();
         e.preventDefault();
-    }else{
+    } else {
         document.getElementById("register-account-tips").style.display = "none";
     }
 
     // 密碼驗證
-    if (newPassword.value.search(passwordReg)==-1){
+    if (newPassword.value.search(passwordReg) == -1) {
         document.getElementById("register-password-tips").innerText = "密碼長度為8~25字元且包含英文大小寫及數字";
-        document.getElementById("register-password-tips").style.color ="red";
-        newPassword.value ="";
+        document.getElementById("register-password-tips").style.color = "red";
+        newPassword.value = "";
         newPassword.select();
         e.preventDefault();
-    }else{
+    } else {
         document.getElementById("register-password-tips").style.display = "none";
     }
 
     // 確認密碼驗證
-    if(confirmPassword.value != newPassword.value){
+    if (confirmPassword.value != newPassword.value) {
         document.getElementById("confirm-id").innerText = "確認密碼錯誤";
         document.getElementById("confirm-id").style.color = "red";
         confirmPassword.value = "";
         confirmPassword.select();
         e.preventDefault();
-    }else{
+    } else {
         document.getElementById("confirm-id").style.display = "none";
     }
 
-    
+
     // 隱私條款打勾驗證
     let privacyTick = $id("privacy");
     // privacyTick.type == "checkbox";
-    if(privacyTick.checked ==false){
+    if (privacyTick.checked == false) {
         console.log(privacyTick.checked);
         alert("請勾選同意隱私條款");
     }
@@ -69,13 +69,16 @@ function registerForm(e){
 
 
     // 如果帳號、密碼、確認密碼和隱私條款都驗證通過，就跳轉到首頁
-    if (AccountValid && PasswordValid && confirmPassword.value == newPassword.value && privacyTick.checked == true) return window.location.replace('/index.html');
+    if (AccountValid && PasswordValid && confirmPassword.value == newPassword.value && privacyTick.checked == true) {
+        alert("恭喜註冊成功!!!");
+        return window.location.replace('/index.html');
+    };
 }
 
 
 
 
 
-window.addEventListener( "load" , function (){
+window.addEventListener("load", function() {
     document.getElementById("memRegister").onsubmit = registerForm;
 });
